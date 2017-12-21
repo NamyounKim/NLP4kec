c8c94c734e41e7311d445166c2d8001d722050f9
NamyounKim <namyun84@gmail.com>
2017-12-21 23:47:11 +0900

    12_21

--- R/RTextParser.R
+++ R/RTextParser.R
@@ -1,4 +1,3 @@
-
 .PkgName = "NLP4kec"
 .onLoad <- function(libname, pkgname){
   library(rJava)
@@ -14,25 +13,52 @@
   .jaddClassPath(dir( file.path(system.file(package = .PkgName),"java"), full.names=TRUE ))
 }
 
-text_parser_file <- function(path, useEn = F, language, korDicPath = "noDic"){
+r_parser_r <- function (contentVector, useEn = F, language, korDicPath = "noDic")
+{
   obj = .jnew("com.namyun.pos.Starter")
-  props_path = file.path(system.file(package = "NLP4kec"),"java")
-
-  if(language == "zh"){
-    .jcall(obj, "S", "rTextParserToFile",as.character(path), as.character(useEn), as.character(language), paste0(props_path,"/StanfordCoreNLP-chinese.properties"), "NULL")
-  }else{
-    .jcall(obj, "S", "rTextParserToFile",as.character(path), as.character(useEn), as.character(language),"NULL", as.character(korDicPath))
+  props_path = file.path(system.file(package = "NLP4kec"), "java")
+  if (language == "zh") {
+    return(.jcall(obj, "[Ljava/lang/String;", "rTextParserFromRtoR",
+                  .jarray(contentVector), as.character(useEn), as.character(language),
+                  paste0(props_path, "/StanfordCoreNLP-chinese.properties"),
+                  "NULL"))
+  }
+  else {
+    return(.jcall(obj, "[Ljava/lang/String;", "rTextParserFromRtoR",
+                  .jarray(contentVector), as.character(useEn), as.character(language),
+                  "NULL", as.character(korDicPath)))
   }
 }
 
-text_parser <- function(path, useEn = F, language, korDicPath = "noDic"){
+file_parser_file <- function(path, useEn = F, language, korDicPath = "noDic"){
   obj = .jnew("com.namyun.pos.Starter")
-  props_path = file.path(system.file(package = "NLP4kec"),"java")
+  props_path = file.path(system.file(package = "NLP4kec"), "java")
+  if (language == "zh") {
+    .jcall(obj, "S", "rTextParserToFile", as.character(path),
+           as.character(useEn), as.character(language), paste0(props_path,
+                                                               "/StanfordCoreNLP-chinese.properties"), "NULL")
+  }
+  else {
+    .jcall(obj, "S", "rTextParserToFile", as.character(path),
+           as.character(useEn), as.character(language), "NULL",
+           as.character(korDicPath))
+  }
+}
 
-  if(language == "zh"){
-    return(.jcall(obj, "[Ljava/lang/String;", "rTextParserToR",as.character(path), as.character(useEn), as.character(language), paste0(props_path,"/StanfordCoreNLP-chinese.properties"), "NULL"))
-  }else{
-    return(.jcall(obj, "[Ljava/lang/String;", "rTextParserToR",as.character(path), as.character(useEn), as.character(language), "NULL", as.character(korDicPath)))
+file_parser_r <- function(path, useEn = F, language, korDicPath = "noDic"){
+  obj = .jnew("com.namyun.pos.Starter")
+  props_path = file.path(system.file(package = "NLP4kec"), "java")
+
+  if (language == "zh") {
+    return(.jcall(obj, "[Ljava/lang/String;", "rTextParserToR",
+                  as.character(path), as.character(useEn), as.character(language),
+                  paste0(props_path, "/StanfordCoreNLP-chinese.properties"),
+                  "NULL"))
+  }
+  else {
+    return(.jcall(obj, "[Ljava/lang/String;", "rTextParserToR",
+                  as.character(path), as.character(useEn), as.character(language),
+                  "NULL", as.character(korDicPath)))
   }
 }
 
